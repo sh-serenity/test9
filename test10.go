159e12a8531e9171621b9fd40f4750c240ea8624
root <root@as-tmp16-ucp-3f7mkxp74bvv-server-p32x3zttvmyc.cluster.local>
2022-12-26 03:51:01 +0000

    7

--- test10.go
+++ test10.go
@@ -7,7 +7,7 @@ import (
     "fmt"
 //    "os"
     "path/filepath"
-
+    "time"
     appsv1 "k8s.io/api/apps/v1"
     apiv1 "k8s.io/api/core/v1"
     metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -15,7 +15,7 @@ import (
     "k8s.io/client-go/tools/clientcmd"
     "k8s.io/client-go/util/homedir"
     "k8s.io/klog"
-    apierrors "k8s.io/apimachinery/pkg/api/errors"
+//    apierrors "k8s.io/apimachinery/pkg/api/errors"
 //    "k8s.io/client-go/util/retry"
     //
     // Uncomment to load all auth plugins
@@ -27,84 +27,95 @@ import (
     // _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
 )
 
+func IsDeploymentReady(deploy *appsv1.Deployment) bool {
+    return deploy.Status.Replicas > 0 &&
+	deploy.Status.UpdatedReplicas == deploy.Status.Replicas &&
+	deploy.Status.ReadyReplicas == deploy.Status.Replicas &&
+	deploy.Status.AvailableReplicas == deploy.Status.Replicas
+}
+
 func main() {
     var kubeconfig *string
     if home := homedir.HomeDir(); home != "" {
-	kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+    kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
     } else {
-	kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+    kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
     }
     flag.Parse()
 
     config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
     if err != nil {
-	panic(err)
+    panic(err)
     }
     clientset, err := kubernetes.NewForConfig(config)
     if err != nil {
-	panic(err)
+    panic(err)
     }
 
     deploymentsClient := clientset.AppsV1().Deployments(apiv1.NamespaceDefault)
 
     deployment := &appsv1.Deployment{
-	ObjectMeta: metav1.ObjectMeta{
-	    Name: "demo",
-	},
-	Spec: appsv1.DeploymentSpec{
+    ObjectMeta: metav1.ObjectMeta{
+        Name: "demo",
+    },
+    Spec: appsv1.DeploymentSpec{
 //	    Replicas: int32Ptr(2),
-	    Selector: &metav1.LabelSelector{
-		MatchLabels: map[string]string{
-		    "app": "demo",
-		},
+        Selector: &metav1.LabelSelector{
+	MatchLabels: map[string]string{
+	    "app": "demo",
+	},
+        },
+        Template: apiv1.PodTemplateSpec{
+	ObjectMeta: metav1.ObjectMeta{
+	    Labels: map[string]string{
+	    "app": "demo",
 	    },
-	    Template: apiv1.PodTemplateSpec{
-		ObjectMeta: metav1.ObjectMeta{
-		    Labels: map[string]string{
-			"app": "demo",
-		    },
-		},
-		Spec: apiv1.PodSpec{
-		    Containers: []apiv1.Container{
-			{
-			    Name:  "name",
-			    Image: "docker-dev-kaas-local.docker.mirantis.net/mirantis/ceph/golang:1.18.5",
-			    Command: []string{
-				"sleep",
-			    },
-			    Args: []string{
-				"10000",
-			    },			    },
-			},
-		    },
-		},
+	},
+	Spec: apiv1.PodSpec{
+	    Containers: []apiv1.Container{
+	    {
+	        Name:  "demo",
+	        Image: "stormstack/sleep:7",
+	    //    Command: []string{
+	    //	"sleep",
+	    //    },
+	    //    Args: []string{
+	    //	"10000",
+	    //    },			    
+	        },
 	    },
-	}
+	    },
+	},
+        },
+    }
     
 
     // Create Deployment
     fmt.Println("Creating deployment...")
     result, err := deploymentsClient.Create(context.TODO(), deployment, metav1.CreateOptions{})
     if err != nil {
-	panic(err)
+    panic(err)
     }
-    if !IsDeploymentReady(deployment) != true {	
     fmt.Printf("Created deployment %q.\n", result.GetObjectMeta().GetName())
-    result, err = clientset.AppsV1().Deployments("default").Get(context.Background(), "demo", metav1.GetOptions{})
-    if err != nil {	
- 	if apierrors.IsNotFound(err) {
-	    // create if there's no ceph-tools deployment
+    for true {
+	time.Sleep(30000 * time.Millisecond)
+	fmt.Println("Ticker stopped")
+	if IsDeploymentReady(deployment)  {	
+	    err := clientset.AppsV1().Deployments("default").Delete(context.Background(), "demo", metav1.DeleteOptions{})
+    if err != nil {
+	fmt.Println(err)
+	}
 	    klog.V(1).Infof("create name  deployment %s/%s", "default", "demo")
 	    _, err = deploymentsClient.Create(context.TODO(), deployment, metav1.CreateOptions{})
 	    if err != nil {
-		fmt.Println(err)
+	    fmt.Println(err)
 	    }
 	} else {
 	    klog.Errorf("failed to get name deployment %s/%s: %v", "default", "demo", err)
 	    if err != nil {
 		fmt.Println(err)
 	    }
-	    }
+	    
 	}
     }
 }
