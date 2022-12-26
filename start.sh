ae4073f20817c642fe97721c9187745e5fd98250
root <root@as-tmp16-ucp-3f7mkxp74bvv-server-p32x3zttvmyc.cluster.local>
2022-12-26 05:41:58 +0000

    15

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-number=12
+number=14
 cp /root/.kube/config ./
 docker build . -t asinitsyn1024/test9:$number
 docker push asinitsyn1024/test9:$number
--- test10.go
+++ test10.go
@@ -100,7 +100,7 @@ func main() {
     for true {
 	time.Sleep(60000 * time.Millisecond)
 	fmt.Println("Ticker stopped")
-	if IsDeploymentReady(deployment)   {	
+	if IsDeploymentReady(deployment) != true  {	
 	    err := clientset.AppsV1().Deployments("default").Delete(context.Background(), "demo", metav1.DeleteOptions{})
     if err != nil {
 	fmt.Println(err)
@@ -110,6 +110,7 @@ func main() {
 	    if err != nil {
 	    fmt.Println(err)
 	    }
+
 	    
 	}
     }
