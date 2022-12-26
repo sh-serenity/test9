d742e3ce68caf8c6ecfd061b4c05e93c118146d2
root <root@as-tmp16-ucp-3f7mkxp74bvv-server-p32x3zttvmyc.cluster.local>
2022-12-26 06:26:28 +0000

    19

--- test10.go
+++ test10.go
@@ -100,7 +100,7 @@ func main() {
     for true {
 	time.Sleep(30000 * time.Millisecond)
 	fmt.Println("Ticker stopped")
-	if IsDeploymentReady(deployment) != true {
+	if IsDeploymentReady(deployment)  {
 	    err := clientset.AppsV1().Deployments("default").Delete(context.Background(), "demo", metav1.DeleteOptions{})
         if err != nil {
 	fmt.Println(err)
